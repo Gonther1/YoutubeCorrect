c370c03b712fae7a7af56d338faf9adb6b34eafe
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-08-28 21:28:31 -0500

    style::lipstick:: Add things to html and css
    Add div in html and i gave styles to an h3

--- script.js
+++ script.js
@@ -28,6 +28,26 @@ const construirMainHead = async () => {
 }
 construirMainHead()
 
+const fillRightSidebar = async () => {
+    let domElement = document.querySelector('#hola')
+    let a=0
+    for (let i = 0; i < 9; i++) {
+        a>=8 ? a=0 :
+        console.log(a)
+        domElement.insertAdjacentHTML('beforeend', /* html */`
+            <div class="side-video-list">
+                <a href="" class="small-thumbail"><img src="images/thumbnail${++a}.png"></a>
+                <div class="vid-info">
+                    <a href="">Best channel that help you to be a web developer</a>
+                    <p>Easy Tutorials</p>
+                    <p>15k Views</p>
+                </div>
+            </div>
+        `)
+    }
+}
+fillRightSidebar()
+
 // ${peticionJson.main.sectionHead.parrafo.map((value) => `
 // <img src=" ${value.imagen} " class="me-2" style="width: 2vh; filter: invert(100%) sepia(0%) saturate(1%) hue-rotate(72deg) brightness(104%) contrast(101%);" alt=""> ${value.texto} <br>
 // `).join(" ")
